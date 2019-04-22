b97e71effacaa19be024a9e8806c65e6939e899c
Nikita Pedorenko <nikitnya1@gmail.com>
2019-04-22 23:14:53 +0300

    Added Pixel class

---
+++ LAB4OP/LAB4OP/Pixel.cs
@@ -0,0 +1,23 @@
+﻿namespace LAB4OP
+{
+    class Pixel
+    {
+        public int R { get; set; }
+        public int G { get; set; }
+        public int B { get; set; }
+
+        public Pixel(int R, int G, int B)
+        {
+            this.R = R;
+            this.G = G;
+            this.B = B;
+        }
+
+        public Pixel(byte[] bgr)
+        {
+            this.R = bgr[2];
+            this.G = bgr[1];
+            this.B = bgr[0];
+        }
+    }
+}
