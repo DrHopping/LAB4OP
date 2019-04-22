3779fca205c9b4d54716dbcdf74f063c8d9cb741
Nikita Pedorenko <nikitnya1@gmail.com>
2019-04-22 23:16:15 +0300

    Added Image class

---
+++ LAB4OP/LAB4OP/Image.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace LAB4OP
+{
+
+    class Image
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int FileSize { get; set; }
+        private byte[] info;
+        public Pixel[] Pixels { get; set; }
+    }
+}
