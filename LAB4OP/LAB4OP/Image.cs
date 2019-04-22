aee6015fd3462ec396e25abc75bf12528f09f0c3
Nikita Pedorenko <nikitnya1@gmail.com>
2019-04-22 23:41:38 +0300

    Added image reading

--- LAB4OP/LAB4OP/Image.cs
+++ LAB4OP/LAB4OP/Image.cs
@@ -17,9 +17,21 @@ namespace LAB4OP
             {
                 info = br.ReadBytes(54);
 
+
                 this.Width = (info[18] | info[19] << 8 | info[20] << 16 | info[21] << 24);
                 this.Height = (info[22] | info[23] << 8 | info[24] << 16 | info[25] << 24);
                 this.FileSize = (info[2] | info[3] << 8 | info[4] << 16 | info[5] << 24);
+
+                Pixel[] pixels = new Pixel[this.Width * this.Height];
+
+                for (int i = 0; i < pixels.Length; i++)
+                {
+                    if(i % this.Width == 0 && i != 0)
+                        br.ReadBytes(2);
+                    pixels[i] = new Pixel(br.ReadBytes(3));
+                }
+
+                this.Pixels = pixels;
             }
         }
     }
--- LAB4OP/LAB4OP/Program.cs
+++ LAB4OP/LAB4OP/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,33 @@ namespace LAB4OP
 {
     class Program
     {
+        static void ShowAllBytes(string path)
+        {
+            using (BinaryReader br = new BinaryReader(File.Open(path, FileMode.Open)))
+            {
+                int i = 1;
+                while (true)
+                {
+
+                    try
+                    {
+                        Console.WriteLine(i + " " + br.ReadByte());
+                        if (i % 3 == 0)
+                            Console.WriteLine("==========");
+                    }
+                    catch { }
+                    i++;
+                }
+            }
+        }
+
+        static string imagePath = @"C:\Users\Nikita Pedorenko\Desktop\image.bmp";
+        static string bmpPath = "bmp10a.bmp";
         static void Main(string[] args)
         {
+      //      ShowAllBytes(bmpPath);
+            Image image = new Image(bmpPath);
+            Console.ReadLine();
         }
     }
 }
