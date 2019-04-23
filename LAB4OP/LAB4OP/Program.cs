795ecd1b1f3bc5c4d5e7f05cbe53597bf5e046af
Nikita Pedorenko <nikitnya1@gmail.com>
2019-04-23 12:43:23 +0300

    Removed menu

--- LAB4OP/LAB4OP/Program.cs
+++ LAB4OP/LAB4OP/Program.cs
@@ -9,21 +9,15 @@ namespace LAB4OP
 {
     class Program
     {
-        static void Menu(string outputPath, int multiplier)
-        {
-            Console.Write($"Enlarging image {multiplier} times...");
-            Console.WriteLine("  Done.");
-            Console.WriteLine($"Written result to {outputPath}");
-
-        }
-
         static void Main(string[] args)
         {
             string inputPath = args[0];
             string outputPath = args[1];
             int multiplier = int.Parse(args[2]);
-            Menu(outputPath, multiplier);
+            Console.Write($"Enlarging image {multiplier} times...");
             (new Image(inputPath)).Enlarge(multiplier).SaveTo(outputPath);
+            Console.WriteLine("  Done.");
+            Console.WriteLine($"Written result to {outputPath}");
         }
     }
 }
