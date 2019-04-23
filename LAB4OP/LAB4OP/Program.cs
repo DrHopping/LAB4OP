f4d209a208fa06947a3ef339bfd94cf1781ee0d8
Nikita Pedorenko <nikitnya1@gmail.com>
2019-04-23 11:33:43 +0300

    Added SaveTo in Image

--- LAB4OP/LAB4OP/Program.cs
+++ LAB4OP/LAB4OP/Program.cs
@@ -29,13 +29,51 @@ namespace LAB4OP
             }
         }
 
+
+        static public byte[] NewInfo(int size)
+        {
+            var newInfo = new byte[4];
+            for (int i = 0; i < 4; i++)
+            {
+                newInfo[i] = (byte)(size & 0xFF);
+                size >>= 8;
+            }
+            return newInfo;
+        }
+
+
+
+        static void CopePaste(string inputPath, string outputPath)
+        {
+            using (BinaryWriter bw = new BinaryWriter(File.Open(outputPath, FileMode.OpenOrCreate)))
+            {
+                using (BinaryReader br = new BinaryReader(File.Open(inputPath, FileMode.Open)))
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            bw.Write(br.ReadByte());
+                        }
+                        catch
+                        {
+                            bw.Close();
+                        }
+                    }
+                }
+            }
+        }
+
         static string imagePath = @"C:\Users\Nikita Pedorenko\Desktop\image.bmp";
         static string bmpPath = "bmp2x2.bmp";
         static void Main(string[] args)
         {
-            //ShowAllBytes(bmpPath);
-            Image image = new Image(bmpPath);
-            List<Pixel> pixels = image.Enlarge(3);
+            //var jo = NewInfo(3000056);
+            //ShowAllBytes("bmp1000.bmp");
+            //CopePaste("bmp1000.bmp", "new.bmp");
+            Image image = new Image("bmp1000.bmp");
+            //List<Pixel> pixels = image.Enlarge(3);
+            image.SaveTo("new2.bmp");
             Console.ReadLine();
         }
     }
